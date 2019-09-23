556ccf4e6115501675d40aa567a05ef234eb8ba9
Kurt Bauer <kurtbauer55@protonmail.com>
2019-09-23 14:32:22 -0400

    add note on init method

--- databases-test/database.py
+++ databases-test/database.py
@@ -3,7 +3,7 @@ class User:
     #pass = if we didn't want our user to do anything
     # self = currently bound object
     def __init__(self, email, first_name, last_name, id): # add args/props of user object, id goes last
-        # init method = allows for definition of props stores in each object
+        # init method = allows for definition of props stores in each object, provide with some values
         self.email = email
         self.first_name = first_name # set props passed into class to be a property of this class
         self.last_name = last_name
