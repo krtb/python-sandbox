86fd2195089b176b597d5334c69528fee3e24dae
Kurt Bauer <kurtbauer55@protonmail.com>
2019-09-23 14:39:52 -0400

    add gitingore for py

--- databases-test/app.py
+++ databases-test/app.py
@@ -1,1 +1,7 @@
-from user import User # from user file, import user class that is in camel case
+from user import User 
+# from user file, import user class that is in camel case
+
+# create variable and set equal to User class instance
+my_user = User('kurt@kurt.me', 'Kurt', 'Bauer', 1) # calls init method and allows it to define the properties, __init__()
+
+print(my_user.email)
