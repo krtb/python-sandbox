6409a5a99406e4d2a2b530a034ba275d9f848283
Kurt Bauer <kurtbauer55@protonmail.com>
2019-09-23 14:29:30 -0400

    add props/arg of User class, leave id at the end

--- databases-test/database.py
+++ databases-test/database.py
@@ -1,6 +1,7 @@
 #create user class
 class User:
     #pass = if we didn't want our user to do anything
-    def __init__(self, *args, **kwargs):
+    # self = currently bound object
+    def __init__(self, email, first_name, last_name, id): # add args/props of user object, id goes last
         # init method = allows for definition of props stores in each object
         super().__init__(*args, **kwargs)
