1e7148e4afebe7f9b85a8ce279eb6860e862a4bc
Kurt Bauer <kurtbauer55@protonmail.com>
2019-09-24 11:49:24 -0400

    succesfully retrieved data from pgsql db

--- databases-test/app.py
+++ databases-test/app.py
@@ -2,7 +2,6 @@ from user import User
 # from user file, import user class that is in camel case
 
 # create variable and set equal to User class instance. instead of (id), has value of NONE, let PSQL handle
-my_user = User('three@three.me', 'three', 'threethree', None) # calls init method and allows it to define the properties, __init__()
-my_user.save_to_db()
+my_user = User.load_from_db_by_email('kurt@kurt.me')
 
 print(my_user)
