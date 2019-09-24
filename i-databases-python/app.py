a14cdb923e4947a28d2f971acd73763a5ef049b3
Kurt Bauer <kurtbauer55@protonmail.com>
2019-09-24 10:50:04 -0400

    remove unused project

--- i-databases-python/app.py
+++
@@ -1,8 +0,0 @@
-from user import User 
-# from user file, import user class that is in camel case
-
-# create variable and set equal to User class instance. instead of (id), has value of NONE, let PSQL handle
-my_user = User('kurt@kurt.me', 'Kurt', 'Bauer', None) # calls init method and allows it to define the properties, __init__()
-
-
-print(my_user)
