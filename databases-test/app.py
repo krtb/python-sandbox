b7160bbe7a5e2bc408bf3bfc5714197c9351f5cc
Kurt Bauer <kurtbauer55@protonmail.com>
2019-09-24 10:54:17 -0400

    remove connection() and close(), packaged with psycopg2

--- databases-test/app.py
+++ databases-test/app.py
@@ -2,7 +2,7 @@ from user import User
 # from user file, import user class that is in camel case
 
 # create variable and set equal to User class instance. instead of (id), has value of NONE, let PSQL handle
-my_user = User('two@two.me', 'two', 'twotwo', None) # calls init method and allows it to define the properties, __init__()
+my_user = User('three@three.me', 'three', 'threethree', None) # calls init method and allows it to define the properties, __init__()
 my_user.save_to_db()
 
 print(my_user)
--- databases-test/user.py
+++ databases-test/user.py
@@ -15,11 +15,10 @@ class User:
         return "<User {}>".format(self.email) # allows your to print the user object with less code
 
     def save_to_db(self): # add method to save user to database
-        connection = psycopg2.connect(user='kurt', password='password', database='learning_python', host='localhost')
+        # change to with/as connection:, psycopg2 not comes with connect and close functions
+        with psycopg2.connect(user='kurt', password='password', database='learning_python', host='localhost') as connection:
         #cursor, retrieves data and reads row by row, have to open this
-        with connection.cursor() as cursor:
-            cursor.execute('INSERT INTO users (email, first_name, last_name) VALUES (%s,%s,%s)',
-                (self.email, self.first_name, self.last_name)
-            ) #does almost the same thing as the format method
-        connection.commit()
-        connection.close()
+            with connection.cursor() as cursor:
+                cursor.execute('INSERT INTO users (email, first_name, last_name) VALUES (%s,%s,%s)',
+                    (self.email, self.first_name, self.last_name)
+                ) #does almost the same thing as the format method
